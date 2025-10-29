8afeff5003337da074b5395244c37bbcca0760e1
Alexkid87IA <alex@nyac.fr>
2025-10-29 19:18:05 +0100

    Updated AppRoutes.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,55 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { usePreroll } from '../contexts/PrerollContext';
+import Preroll from '../components/Preroll/Preroll';
 import HeaderNav from '../components/Nav/HeaderNav';
 import MobileBurger from '../components/Nav/MobileBurger';
+import SectionNavigator from '../components/Navigation/SectionNavigator';
 import Hero from '../sections/Hero';
 import Gallery from '../sections/Gallery';
-import Services from '../sections/Services';
-import Community from '../sections/Community';
+import CoworkingSection from '../sections/ServiceSections/CoworkingSection';
+import DomiciliationSection from '../sections/ServiceSections/DomiciliationSection';
+import BureauxSection from '../sections/ServiceSections/BureauxSection';
+import StudiosSection from '../sections/ServiceSections/StudiosSection';
+import CommunitySection from '../sections/ServiceSections/CommunitySection';
 import Footer from '../components/Footer';
 
-export default function Home() {
+export default function HomeNew() {
+  const { showPreroll, selectedService, handleServiceSelect, handleSkipPreroll } = usePreroll();
+  const hasScrolledToSection = useRef(false);
+
+  useEffect(() => {
+    if (selectedService && !hasScrolledToSection.current) {
+      hasScrolledToSection.current = true;
+      setTimeout(() => {
+        const element = document.getElementById(selectedService);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+      }, 100);
+    }
+  }, [selectedService]);
+
   return (
-    <div className="min-h-screen bg-[#0F172A]">
+    <div className="min-h-screen bg-[#0A0A0A]">
+      {showPreroll && (
+        <Preroll
+          onSelect={handleServiceSelect}
+          onSkip={handleSkipPreroll}
+        />
+      )}
+
       <HeaderNav />
       <MobileBurger />
+      <SectionNavigator />
 
       <main className="pt-24">
         <Hero />
         <Gallery />
-        <Services />
-        <Community />
+        <DomiciliationSection />
+        <BureauxSection />
+        <CoworkingSection />
+        <StudiosSection />
+        <CommunitySection />
       </main>
 
       <Footer />
--- src/router/AppRoutes.tsx
+++ src/router/AppRoutes.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home.new';
+import Home from '../pages/Home';
 import Offres from '../pages/Offres';
 import Tarifs from '../pages/Tarifs';
 import BureauxComparison from '../pages/Bureaux';
