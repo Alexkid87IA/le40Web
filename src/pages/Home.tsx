4da25df319114d03a880c2b3350d4bbf25c2323a
Alexkid87IA <alex@nyac.fr>
2025-10-30 09:04:51 +0100

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,4 @@
-import React, { useEffect, useRef } from 'react';
-import { usePreroll } from '../contexts/PrerollContext';
-import Preroll from '../components/Preroll/Preroll';
+import React from 'react';
 import HeaderNav from '../components/Nav/HeaderNav';
 import MobileBurger from '../components/Nav/MobileBurger';
 import SectionNavigator from '../components/Navigation/SectionNavigator';
@@ -14,30 +12,8 @@ import CommunitySection from '../sections/ServiceSections/CommunitySection';
 import Footer from '../components/Footer';
 
 export default function HomeNew() {
-  const { showPreroll, selectedService, handleServiceSelect, handleSkipPreroll } = usePreroll();
-  const hasScrolledToSection = useRef(false);
-
-  useEffect(() => {
-    if (selectedService && !hasScrolledToSection.current) {
-      hasScrolledToSection.current = true;
-      setTimeout(() => {
-        const element = document.getElementById(selectedService);
-        if (element) {
-          element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        }
-      }, 100);
-    }
-  }, [selectedService]);
-
   return (
     <div className="min-h-screen bg-[#0A0A0A]">
-      {showPreroll && (
-        <Preroll
-          onSelect={handleServiceSelect}
-          onSkip={handleSkipPreroll}
-        />
-      )}
-
       <HeaderNav />
       <MobileBurger />
       <SectionNavigator />
