1fe2badebfd3c56e9a1109d61ef55297f105ba35
Alexkid87IA <alex@nyac.fr>
2025-10-30 19:03:50 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import AppRoutes from './router/AppRoutes';
 import { CartProvider } from './contexts/CartContext';
 import { PrerollProvider } from './contexts/PrerollContext';
-import WhatsAppWidget from './components/UI/WhatsAppWidget';
 
 function App() {
   return (
     <PrerollProvider>
       <CartProvider>
         <AppRoutes />
-        <WhatsAppWidget />
       </CartProvider>
     </PrerollProvider>
   );
