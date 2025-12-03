7d33cab57998673745632e64553aa5e7042a9e99
Alexkid87IA <alex@nyac.fr>
2025-12-03 18:44:19 +0100

    Updated ScrollToTop.tsx

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -2,11 +2,18 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 export default function ScrollToTop() {
-  const { pathname } = useLocation();
+  const { pathname, hash } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    // Si pas de hash dans l'URL, scroll en haut
+    if (!hash) {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'instant' as ScrollBehavior
+      });
+    }
+  }, [pathname, hash]);
 
   return null;
 }
